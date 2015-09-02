1c87087a939590c1a6707071250a873631558c68
Nicholas Rego <nick@seas0918.wireless-pennnet.upenn.edu>
2015-09-02 15:00:39 -0400

    some wham functionality

--- wham.py
+++ wham.py
@@ -6,54 +6,28 @@ from datareader import dr
 
 import matplotlib as mpl
 
+log = logging.getLogger('wham')
 
+def parseRange(rangestr):
+    spl = rangestr.split(",")
+    return tuple([float(i) for i in spl])
 
-def genHistMatrix(S, M):
+# Generate S x M count histogram over Ntwid for all sims
+# (M is number of bins)
+def genHistMatrix(S, M, rng, start):
 
     histMat = numpy.empty((S,M))
-
+    binbounds = numpy.empty((1,M+1))
     for i, ds in enumerate(dr.datasets.itervalues()):
-        pass
+        dataframe = ds.data[start:]['$\~N$'] # make this dynamic in future
+        counts, binbounds = numpy.histogram(dataframe, bins=M, range=rng)
+        histMat[i,:] = counts
 
-    return histMat
+    return histMat, binbounds
 
-
-if __name__ == "__main__":
-
-    parser = argparse.ArgumentParser(description="Run WHAM on collection of INDUS output datasets")
-
-    parser.add_argument('input', metavar='INPUT', type=str, nargs='+',
-                        help='Input file names (presumably in a sensible order)')
-    parser.add_argument('-b', '--start', type=int, default=0,
-                        help='first timepoint (in ps)')
-    parser.add_argument('--debug', action='store_true',
-                        help='print debugging info')
-    parser.add_argument('-T', metavar='TEMP', type=float,
-                        help='convert Phi values to kT, for TEMP (K)')
-    parser.add_argument('-nbins', metavar='NBINS', type=int, default=50,
-                        help='Number of histogram bins')
-
-    log = logging.getLogger()
-    log.addHandler(logging.StreamHandler())
-
-    args = parser.parse_args()
-
-    if args.debug:
-        log.setLevel(logging.DEBUG)
-
-    infiles = args.input
-
-    log.debug("{} input files".format(len(infiles)))
-    start = args.start
-
-    conv = 1
-    if args.T:
-        conv /= (args.T * 0.008314)
-
-    S = len(infiles) # Assume number of simulations from input
-
-    nbins = args.nbins
-
+# Load list of infiles from start
+# Return range over entire dataset (as tuple)
+def loadRangeData(infiles, start):
     minval = float('inf')
     maxval = float('-inf')
     for f in infiles:
@@ -66,11 +40,63 @@ if __name__ == "__main__":
         minval = tmpmin if minval>tmpmin else minval
         maxval = tmpmax if maxval<tmpmax else maxval
 
+    return minval, maxval    
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="Run WHAM on collection of INDUS output datasets")
+
+    parser.add_argument('input', metavar='INPUT', type=str, nargs='+',
+                        help='Input file names (presumably in a sensible order)')
+    parser.add_argument('-b', '--start', type=int, default=0,
+                        help='first timepoint (in ps)')
+    parser.add_argument('--debug', action='store_true',
+                        help='print debugging info')
+    parser.add_argument('-v', '--verbose', action='store_true',
+                        help='be verbose')
+    parser.add_argument('-T', metavar='TEMP', type=float,
+                        help='convert Phi values to kT, for TEMP (K)')
+    parser.add_argument('-nbins', metavar='NBINS', type=int, default=50,
+                        help='Number of histogram bins (over range if specified, or from \
+                            the minium to maximum of the input[start:]')
+    parser.add_argument('--range', type=str,
+                        help="Specify custom data range (as 'minval,maxval') for \
+                        histogram binning (default: Full data range)")
+
+    
+
+    args = parser.parse_args()
+
+    if args.verbose:
+        log.setLevel(logging.INFO)
+    if args.debug:
+        log.setLevel(logging.DEBUG)
+
+    infiles = args.input
+
+    log.info("{} input files".format(len(infiles)))
+    start = args.start
+
+    conv = 1
+    if args.T:
+        conv /= (args.T * 0.008314)
+
+    S = len(infiles) # Assume number of simulations from input
+
+    nbins = args.nbins
+
     # N twid range
-    data_range = maxval - minval
+    data_range = loadRangeData(infiles, start)
 
-    log.debug('max: {} min: {} range: {}'.format(minval, maxval, data_range))
+    if args.range:
+        data_range = parseRange(args.range)
 
-    histMat = genHistMatrix(S, nbins)
+    log.info('max, min: {}'.format(data_range))
+
+    histMat, binbounds = genHistMatrix(S, nbins, data_range, start)
+
+    log.info('Hist map shape: {}'.format(histMat.shape))
+    log.debug('Bin bounds over range: {}'.format(binbounds))
 
 
