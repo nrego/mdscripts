96888c075c93a4d4e9847aea1e4feba14da03d64
Nicholas Rego <nick@SEAS444.wlan.seas.upenn.edu>
2016-08-15 13:50:30 -0400

    new bootstrapping for phierr

--- gen_umbr.py
+++ gen_umbr.py
@@ -61,7 +61,7 @@ if __name__ == "__main__":
 
         if args.static:
             for atm in prot_heavies:
-                fout.write("{:<10.1f} {:<10.1f} {:<10.3f} {:<10.3f} {:<10.3f}\\\n".format(-0.5, args.rad/10.0, atm.pos[0], atm.pos[1], atm.pos[2]))
+                fout.write("{:<10.1f} {:<10.1f} {:<10.3f} {:<10.3f} {:<10.3f}\\\n".format(-0.5, args.rad/10.0, atm.pos[0]/10.0, atm.pos[1]/10.0, atm.pos[2]/10.0))
         else:
             for atm in prot_heavies:
                 fout.write("{:<10.1f} {:<10.1f} {:d} \\\n".format(-0.5, args.rad/10.0, atm.index+1))
@@ -112,7 +112,7 @@ if __name__ == "__main__":
                 for atomidx in xrange(n_waters.shape[0]):
                     if n_waters[atomidx] > args.avgwater:
                         atm = prot_heavies[atomidx]
-                        fout.write("{:<10.1f} {:<10.1f} {:<10.3f} {:<10.3f} {:<10.3f}\\\n".format(-0.5, args.rad/10.0, atm.pos[0], atm.pos[1], atm.pos[2]))                   
+                        fout.write("{:<10.1f} {:<10.1f} {:<10.3f} {:<10.3f} {:<10.3f}\\\n".format(-0.5, args.rad/10.0, atm.pos[0]/10.0, atm.pos[1]/10.0, atm.pos[2]/10.0))                   
             else:
                 for atomidx in xrange(n_waters.shape[0]):
                     if n_waters[atomidx] > args.avgwater:
