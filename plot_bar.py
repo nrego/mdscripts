3767b4c4a847e90a2356f54274d772e9842f679b
Nick Reg <nrego@med.upenn.edu>
2018-01-29 14:58:56 -0500

    blah

--- plot_bar.py
+++ plot_bar.py
@@ -9,16 +9,16 @@ mpl.rcParams.update({'xtick.labelsize': 30})
 mpl.rcParams.update({'ytick.labelsize': 30})
 mpl.rcParams.update({'axes.titlesize': 40})
 
-
-
 fig, ax = plt.subplots()
-ind = np.arange(3)
+ind = np.arange(28)
 width = 1
-rects1 = ax.bar(ind, ddg_cav, width=width, edgecolor='k', color='b')
-ax.set_ylabel(r'$ \Delta G_{\rm{cav}} \; (k_B T)$')
-ax.set_xticks(ind)
-ax.set_xticklabels(('L07S', 'L19S', 'L21S'))
-#ax.legend((rects1[0], rects2[0]), ('bind', 'cav'))
+rects1 = ax.bar(ind[::4], dat[:,0], width=width, edgecolor='k', color='y')
+ax.set_ylabel(r'$\Delta \Delta G \; (k_B T)$')
+ax.set_xticks(ind[1::4])
+labels = ('Ala', 'Val', 'Leu', 'Ile', 'Ser', 'Thr', 'Asn')
+ax.set_xticklabels(labels)
 
-#rects2 = ax.bar(ind+width, cav_unbound[1:] - cav_unbound[0], width=width, edgecolor='k', color='r')
+rects2 = ax.bar(ind[1::4], dat[:,1], width=width, edgecolor='k', color='g')
+rects3 = ax.bar(ind[2::4], dat[:,2], width=width, edgecolor='k', color='b')
+ax.legend((rects1[0], rects2[0], rects3[0]), (r'$\Delta \Delta G$', r'$\Delta \Delta G_{\rm{SR}}$', r'$\Delta \Delta G_{\rm{ins}}$'))
 
