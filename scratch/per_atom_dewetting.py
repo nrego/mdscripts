a253ec49f064807e0d409e58da3e910c2e4ef5a3
Nick Rego <nbrego2@gmail.com>
2018-05-01 15:43:06 -0400

    remove buried mask nonsense from per_atom_dewetting

--- scratch/per_atom_dewetting.py
+++ scratch/per_atom_dewetting.py
@@ -14,6 +14,9 @@ univ = MDAnalysis.Universe('phi_000/dynamic_volume_water_avg.pdb')
 prot_atoms = univ.atoms
 assert base_prot_group.n_atoms == prot_atoms.n_atoms
 
+# weight for calculating per-atom norm density
+wt = 0.5 
+
 # Note: 0-indexed!
 all_atom_lookup = base_prot_group.indices # Lookup table from prot_atoms index to all-H index
 atm_indices = prot_atoms.indices
@@ -21,7 +24,7 @@ atm_indices = prot_atoms.indices
 max_water = 33.0 * (4./3.)*np.pi*(0.6)**3
 
 # To determine if atom buried (if avg waters fewer than thresh) or not
-avg_water_thresh = 7
+avg_water_thresh = -1
 # atom is 'dewet' if its average number of is less than this percent of
 #   its value at phi=0
 per_dewetting_thresh = 0.6
@@ -39,15 +42,7 @@ n_frames = rho_water0.shape[1]
 rho_avg_solute0 = rho_solute0.mean(axis=1)
 rho_avg_water0 = rho_water0.mean(axis=1)
 
-# Find surface atoms...
-surf_mask = rho_avg_water0 > avg_water_thresh
-np.savetxt('surf_mask.dat', surf_mask, fmt='%d')
-rho_solute0 = rho_solute0[surf_mask]
-rho_water0 = rho_water0[surf_mask]
-rho_avg_solute0 = rho_avg_solute0[surf_mask]
-rho_avg_water0 = rho_avg_water0[surf_mask]
-
-surf_indices = atm_indices[surf_mask]
+max_solute = rho_avg_solute0.max()
 
 # variance and covariance
 delta_rho_water0 = rho_water0 - rho_avg_water0[:,np.newaxis]
@@ -66,14 +61,9 @@ for fpath in paths:
     all_prot_atoms = all_h_univ.select_atoms('protein and not name H*')
     univ = MDAnalysis.Universe('{}/dynamic_volume_water_avg.pdb'.format(dirname))
 
-    # mask the buried atoms
-    univ.atoms[~surf_mask].bfactors = max_water
-    all_prot_atoms[~surf_mask].bfactors = max_water
-    univ.atoms.write('{}/surf_masked.pdb'.format(dirname))
-
     ds = np.load(fpath)
-    rho_solute = (ds['rho_solute'].T)[surf_mask]
-    rho_water = (ds['rho_water'].T)[surf_mask]
+    rho_solute = (ds['rho_solute'].T)
+    rho_water = (ds['rho_water'].T)
     del ds
 
     assert rho_water.shape[0] == surf_mask.sum()
@@ -84,25 +74,20 @@ for fpath in paths:
     rho_avg_solute = rho_solute.mean(axis=1)
     rho_avg_water = rho_water.mean(axis=1)
 
-    # percentage each atom is dewet (w.r.t. its phi=0.0 value)
-    per_dewet = (rho_avg_water/rho_avg_water0)
-    univ.atoms[surf_mask].bfactors = per_dewet
-    all_prot_atoms[surf_mask].bfactors = per_dewet
-    univ.atoms.write('{}/per_dewet.pdb'.format(dirname))
-    all_h_univ.atoms.write('{}/all_per_dewet.pdb'.format(dirname))
-
-    dewet_mask = per_dewet < per_dewetting_thresh
+    # New, normalized water and protein density
+    #   hopefully accounts for case when exposed atom becomes buried at phi>0
+    #   (and would possibly be a false dewet positive otherwise)
+    per_dewet = wt*(rho_avg_water/rho_avg_water0) + (1-wt)*(rho_avg_solute/rho_avg_solute0)
+    rho_norm = (rho_avg_water/max_water) + (rho_avg_solute/max_solute)
+    univ.atoms.bfactors = per_dewet
+    univ.atoms.write('per_dewet.pdb')
+    univ.atoms.bfactors = rho_norm
+    univ.atoms.write('norm.pdb')
+    dewet_mask = per_dewet < 0.5
     univ.atoms.bfactors = 1
-    all_prot_atoms.bfactors = 1
-    univ.atoms[surf_mask][dewet_mask].bfactors = 0
-    all_prot_atoms[surf_mask][dewet_mask].bfactors = 0
-    univ.atoms.write('{}/dewet_atoms.pdb'.format(dirname))
-    all_h_univ.atoms.write('{}/all_dewet_atoms.pdb'.format(dirname))
+    univ.atoms[dewet_mask].bfactors = 0
 
-    print("dir: {}  n_dewet: {} of {}".format(dirname, dewet_mask.sum(), surf_mask.sum()))
-
-
-
+    print("dir: {}  n_dewet: {} of {}".format(dirname, dewet_mask.sum(), prot_atoms.n_atoms))
 
 
 
