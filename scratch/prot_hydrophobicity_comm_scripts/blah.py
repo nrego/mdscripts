9abb082dc8a42e7c52573818f9ce538f028428ca
Nick Rego <nickrego@nick-iMac.local>
2018-08-23 12:24:27 -0400

    prot scratch scripts

--- scratch/prot_hydrophobicity_comm_scripts/blah.py
+++ scratch/prot_hydrophobicity_comm_scripts/blah.py
@@ -1,9 +1,22 @@
 
 import numpy as np
 from whamutils import get_neglogpdist
+from constants import k
+
+kt = k*300
+beta = 1/kt
 
 def plt_errorbars(bb, loghist, errs):
     plt.fill_between(bb[:-1], loghist-errs, loghist+errs, alpha=0.5)
+
+def window_smoothing(arr, windowsize=5):
+    ret_arr = np.zeros_like(arr)
+    ret_arr[:] = float('nan')
+
+    for i in range(ret_arr.size):
+        ret_arr[i] = arr[i-windowsize:i+windowsize].mean()
+
+    return ret_arr
 
 boot_dat = np.load('boot_fn_payload.dat.npy')
 n_boot = boot_dat.shape[0]
@@ -11,23 +24,49 @@ n_boot = boot_dat.shape[0]
 logweights_0, dat_0, dat_N_0 = boot_dat[0]
 bb = np.arange(0,2000,1.)
 
+
 boot_neglogpdist = np.zeros((n_boot, bb.size-1))
+boot_neglogpdist_N = np.zeros_like(boot_neglogpdist)
+boot_fprime = np.zeros((n_boot, bb.size-2))
+
+max_N = 0
 
 for i, payload in enumerate(boot_dat):
-
+    #print("doing {}".format(i))
     logweights, all_dat, all_dat_N = payload
 
-    neglogpdist = get_neglogpdist(all_dat.astype(float), bb, logweights)
-    boot_neglogpdist[i] = neglogpdist
+    max_N = np.ceil(max(max_N, np.max((all_dat, all_dat_N)))) + 1
 
-nomask = boot_neglogpdist
-boot_neglogpdist = np.ma.masked_invalid(boot_neglogpdist)
+    neglogpdist = get_neglogpdist(all_dat.astype(float), bb, logweights)
+    neglogpdist_N = get_neglogpdist(all_dat_N.astype(float), bb, logweights)
+    smooth_pdist = window_smoothing(neglogpdist)
+
+    boot_neglogpdist[i] = neglogpdist
+    boot_neglogpdist_N[i] = neglogpdist_N
+    boot_fprime[i] = np.diff(smooth_pdist)
+
+range_mask = bb < max_N
+
+bb = bb[range_mask]
+range_mask = range_mask[:-1]
+
+boot_neglogpdist = np.ma.masked_invalid(boot_neglogpdist[:, range_mask])
 avg_neglogpdist = boot_neglogpdist.mean(axis=0)
 err_neglogpdist = boot_neglogpdist.std(axis=0, ddof=1)
 mask = avg_neglogpdist.mask
 avg_neglogpdist.data[mask] = 'inf'
-outarr = np.vstack((bb[:-1], avg_neglogpdist, err_neglogpdist)).T
+
+outarr = np.vstack((bb, avg_neglogpdist, err_neglogpdist)).T
 np.savetxt('neglogpdist.dat', outarr)
+
+boot_neglogpdist_N = np.ma.masked_invalid(boot_neglogpdist_N[:, range_mask])
+avg_neglogpdist_N = boot_neglogpdist_N.mean(axis=0)
+err_neglogpdist_N = boot_neglogpdist_N.std(axis=0, ddof=1)
+mask = avg_neglogpdist_N.mask
+avg_neglogpdist_N.data[mask] = 'inf'
+
+outarr_N = np.vstack((bb, avg_neglogpdist_N, err_neglogpdist_N)).T
+np.savetxt('neglogpdist_N.dat', outarr_N)
 
 
 
---
+++ scratch/prot_hydrophobicity_comm_scripts/plot_comparison.py
@@ -0,0 +1,123 @@
+import os, glob
+
+from constants import k
+import matplotlib as mpl
+
+mpl.rcParams.update({'axes.labelsize': 20})
+mpl.rcParams.update({'xtick.labelsize': 20})
+mpl.rcParams.update({'ytick.labelsize': 20})
+mpl.rcParams.update({'axes.titlesize': 25})
+mpl.rcParams.update({'legend.fontsize':18})
+
+kt = 300*k
+beta = 1 / kt
+
+
+
+def window_smoothing(arr, windowsize=10):
+    ret_arr = np.zeros_like(arr)
+    ret_arr[:] = float('nan')
+
+    for i in range(ret_arr.size):
+        ret_arr[i] = arr[i-windowsize:i+windowsize].mean()
+
+    return ret_arr
+
+
+fnames = sorted( glob.glob('rv*/neglogpdist.dat') )
+phi_vals = beta*np.arange(0, 10, 0.1)
+
+for f in reversed(fnames):
+    dirname = os.path.dirname(f)
+
+    #rv = int(dirname.split('_')[-1]) / 10
+    #title_header = '/Users/nickrego/Desktop/ubiq_rv_{:02d}'.format(rv)
+    #title_header = '/Users/nickrego/Desktop/bulk_rv_10'
+    title_header = '/Users/nickrego/Desktop/{}'.format(dirname)
+    rv=0
+    dat = np.loadtxt(f)
+
+    n_vals = dat[:,0]
+    neglogpdist = dat[:,1]
+
+    window = 5
+    smooth = window_smoothing(neglogpdist, windowsize=window)
+
+    fin_diff = np.diff(smooth)
+    
+    plt.plot(n_vals, neglogpdist)
+    plt.xlabel(r'$N$')
+    plt.ylabel(r'$\beta F_V(N)$')
+    plt.tight_layout()
+    plt.savefig('{}_F.pdf'.format(title_header))
+    plt.show()
+
+    plt.plot(n_vals[1:], fin_diff)
+    plt.xlabel(r'$N$')
+    plt.ylabel(r'$\frac{\partial \beta F_V(N)}{\partial N}$')
+    ymin, ymax = plt.ylim()
+    plt.ylim(ymin, 0)
+    plt.tight_layout()
+    plt.savefig('{}_Fprime.pdf'.format(title_header))
+    plt.show()
+
+    curv = np.diff(window_smoothing(fin_diff, 5))
+    plt.plot(n_vals[2:], curv)
+    plt.xlabel(r'$N$')
+    plt.ylabel(r'$\frac{\partial^2 \beta F_V(N)}{\partial N^2}$')
+    plt.tight_layout()
+    plt.savefig('{}_Fcurv.pdf'.format(title_header))
+    plt.show()
+
+    avg_ns = []
+    var_ns = []
+    for phi in phi_vals:
+        bias = n_vals*phi + neglogpdist
+        bias -= bias.min()
+        pdist = np.exp(-bias)
+        pdist /= pdist.sum()
+
+        avg_n = np.trapz(pdist*n_vals, n_vals)
+        avg_nsq = np.trapz(pdist*n_vals**2, n_vals)
+        var_n = avg_nsq - avg_n**2
+
+        avg_ns.append(avg_n)
+        var_ns.append(var_n)
+
+    avg_ns = np.array(avg_ns)
+    var_ns = np.array(var_ns)
+
+
+    max_idx = np.argmax(var_ns)
+
+    phi_star = phi_vals[max_idx]
+
+    plt.plot(phi_vals[:max_idx+20], avg_ns[:max_idx+20])
+    plt.xlabel(r'$\beta \phi$')
+    plt.ylabel(r'$\langle N_V \rangle_\phi$')
+    plt.tight_layout()
+    plt.savefig('{}_n_v_phi.pdf'.format(title_header))
+    plt.show()
+
+    plt.plot(phi_vals[:max_idx+20], var_ns[:max_idx+20])
+    plt.xlabel(r'$\beta \phi$')
+    plt.ylabel(r'$\langle \delta N^2_V \rangle_\phi$')
+    plt.tight_layout()
+    plt.savefig('{}_sus_v_phi.pdf'.format(title_header))
+    plt.show()
+
+    bias = n_vals*phi_star + neglogpdist
+    bias -= bias.min()
+
+    plt.plot(n_vals, neglogpdist, label=r'$\beta \phi=0$')
+    plt.plot(n_vals, bias, label=r'$\beta \phi^*={:0.2f}$'.format(phi_star))
+    plt.legend()
+    plt.ylim(0,12)
+    plt.xlabel(r'$N$')
+    plt.ylabel(r'$\beta F^\phi(N)$')
+    plt.tight_layout()
+    plt.savefig('{}_Fphistar.pdf'.format(title_header))
+    plt.show()
+
+#plt.legend()
+#plt.show()
