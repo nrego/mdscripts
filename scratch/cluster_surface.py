55bddd04a7cfd24e732ccba64d7ce9da33169500
Nick Rego <nbrego2@gmail.com>
2018-08-29 16:22:09 -0400

    surf clust

--- scratch/cluster_surface.py
+++ scratch/cluster_surface.py
@@ -10,8 +10,7 @@ from IPython import embed
 import sklearn.cluster
 from sklearn import metrics
 from sklearn.preprocessing import StandardScaler
-from sklearn.neighbors import kneighbors_graph
-
+from sklearn.neighbors import NearestNeighbors
 
 univ = MDAnalysis.Universe('prot_heavies_by_charge.pdb')
 mask = univ.atoms.tempfactors == 0 # Surface hydrophobic atoms
@@ -19,34 +18,35 @@ mask = univ.atoms.tempfactors == 0 # Surface hydrophobic atoms
 surf = univ.atoms[mask]
 surf_pos = surf.positions.copy()
 
-bandwidth = sklearn.cluster.estimate_bandwidth(surf_pos, quantile=0.3)
+n_tot = surf.n_atoms
 
-connectivity = kneighbors_graph(surf_pos, n_neighbors=10, include_self=False)
+nn = NearestNeighbors(n_neighbors=6)
+nn.fit(surf_pos)
+dist, ind = nn.kneighbors()
 
-scores = []
+# The distance of each point to its 6th nearest neighbor
+#   (in decreasing order)
+dist = np.sort(dist[:,-1])[::-1]
 
+plt.plot(dist)
+plt.show()
 
-
-for eps in np.arange(0.5, 5.5, 0.5):
-    for min_samples in np.arange(5, 13, 1):
+for eps in np.arange(4.0, 6.5, 0.5):
+    for min_samples in np.arange(5,20,1):
         labels = sklearn.cluster.DBSCAN(eps=eps, min_samples=min_samples).fit(surf_pos).labels_
-        print("eps: {} n: {}  k: {}".format(eps, min_samples, np.unique(labels).size))
+        
+        n_noise = (labels == -1).sum()
+        clust_labels = labels[labels!=-1]
+        n_clust = np.unique(clust_labels).size
+        hist, _ = np.histogram(clust_labels, bins=np.arange(n_clust+1))
+        try:
+            n_largest_patch = hist.max()
+        except ValueError:
+            n_largest_patch = 0
 
-embed()
-'''
-kvals = range(2,100)
-for k in kvals:
-    clust = sklearn.cluster.AgglomerativeClustering(n_clusters=k, linkage='ward', connectivity=connectivity)
-    clust.fit(surf_pos)
-    labels = clust.labels_
-    s = metrics.silhouette_score(surf_pos, labels, metric='euclidean')
-    print("k: {}, s: {}".format(k,s))
-    scores.append(s)
-
-scores = np.array(scores)
-
-max_idx = np.argmax(scores)
-print('best performance: k={} clusters'.format(kvals[max_idx]))
-'''
+        print("eps: {} min_n: {}  n_patch: {} largest_patch: {:0.2f} noise: {:0.2f}".format(eps, min_samples, n_clust, n_largest_patch/n_tot, n_noise/n_tot))
 
 
+labels = sklearn.cluster.DBSCAN(eps=4, min_samples=5).fit(surf_pos).labels_
+surf.tempfactors = labels
+surf.write('surf.pdb')
