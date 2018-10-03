f3dfe7f4e4bcc552c478b925373e248f52ff12a0
Nick Rego <nickrego@nick-iMac.local>
2018-10-03 15:43:38 -0400

    blah

--- scratch/prot_pred/find_contact.py
+++ scratch/prot_pred/find_contact.py
@@ -21,9 +21,21 @@ parser.add_argument('--rhodata', type=str, required=True,
                     help='rho_data_dump file for which to find dewetted atoms')
 parser.add_argument('-nb', default=5, type=float,
                     help='Solvent exposure criterion for determining buried atoms from reference')
+parser.add_argument('--thresh', default=0.5, type=float,
+                    help='Threshold for determining if atom is dewetted, after normalizing by ref')
 
 args = parser.parse_args()
 
 sys = MDSystem(args.topology, args.struct)
 
-embed()
+rho_ref = np.load(args.ref)['rho_water'].mean(axis=0)
+rho_targ = np.load(args.rhodata)['rho_water'].mean(axis=0)
+dewet_mask = (rho_targ/rho_ref) < args.thresh
+sys.find_buried(rho_ref, nb=args.nb)
+surf_mask = sys.surf_mask_h
+
+surf_dewet_mask = surf_mask & dewet_mask
+
+sys.prot_h[surf_dewet_mask].tempfactors = 1
+sys.prot_h.write('surf_dewet.pdb')
+
