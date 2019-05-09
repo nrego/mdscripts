8c7da1d62dfab3bb5f29c86443f38d190ab3bb90
Nick Rego <nbrego2@gmail.com>
2019-05-09 17:51:42 -0400

    blah

--- scratch/sam/blah2.py
+++ scratch/sam/blah2.py
@@ -1,40 +1,79 @@
+from __future__ import division, print_function
+
 import os, glob
 from scipy import special
 from util import *
 
+import matplotlib as mpl
 
-pos_ext = gen_pos_grid(12, z_offset=True, shift_y=-3, shift_z=-3)
-coef = np.array([-1.66049927, -0.46594186, -0.17488402])
+fnames = glob.glob('pattern_sample/*/d_*/trial_0/PvN.dat')
+fnames = np.append(fnames, glob.glob('inv_pattern_sample/*/d_*/trial_0/PvN.dat'))
 
-pred = []
-for n in range(2,7):
-    positions = gen_pos_grid(n)
+nvphis = []
+pvns = []
+phistars = []
+fes = []
+ndats = []
+kvals = []
+peak_vars = []
+for fname in fnames:
 
-    # patch_idx is list of patch indices in pos_ext 
-    #   (pos_ext[patch_indices[i]] will give position[i], ith patch point)
-    d, patch_indices = cKDTree(pos_ext).query(positions, k=1)
+    is_k = fname.split('/')[1].split('_')[0] == 'k'
+    number = int(fname.split('/')[1].split('_')[1])
+    if is_k:
+        kval = number
+    else:
+        kval = 36 - number
 
-    # nn_ext is dictionary of (global) nearest neighbor's to each patch point
-    #   nn_ext[i]  global idxs of neighbor to local patch i 
-    nn, nn_ext, dd, dd_ext = construct_neighbor_dist_lists(positions, pos_ext)
+    kvals.append(kval)
 
-    edges, ext_indices = enumerate_edges(positions, pos_ext, nn_ext, patch_indices)
-    n_edges = edges.shape[0]
-    int_indices = np.setdiff1d(np.arange(n_edges), ext_indices)
+    dirname = os.path.dirname(fname)
+    nvphi = np.loadtxt('{}/NvPhi.dat'.format(dirname))
+    #ndat = np.loadtxt('{}/n_out.dat'.format(dirname))
+    pvn = np.loadtxt(fname)
 
-    methyl_mask = np.ones(n*n, dtype=bool)
-    k_eff_all = get_keff_all(methyl_mask, edges, patch_indices)
+    nvphis.append(nvphi)
+    pvns.append(pvn)
+    fe = pvn[0,1]
+    fes.append(fe)
+    phistar_idx = np.argmax(nvphi[:,4])
+    var_0 = nvphi[0, 4]
+    peak_var = nvphi[phistar_idx, 4]
+    peak_vars.append(peak_var/var_0)
+    phistar = nvphi[phistar_idx, 0]
 
-    int_edges = k_eff_all[int_indices].sum(axis=0)
-    ext_edges = k_eff_all[ext_indices].sum(axis=0)
+    phistars.append(phistar)
 
-    n_mm_int = int_edges[0]
-    n_mo_int = int_edges[2]
-    n_mo_ext = ext_edges[-2]
+    #ndats.append(ndat)
 
-    delta_g = np.dot(coef, np.array([n_mm_int, n_mo_int, n_mo_ext]))
-    pred.append(delta_g)
+phistars = np.array(phistars)
+fes = np.array(fes)
+nvphis = np.array(nvphis)
+ndats = np.array(ndats)
 
-    print("n: {:01d}   dg: {:0.2f}".format(n, delta_g))
+peak_vars = np.array(peak_vars)
+sort_idx = np.argsort(phistars)
+plt.close('all')
 
-pred = np.array(pred)
+norm = plt.Normalize(1, np.ceil(peak_vars.max()))
+cmap = mpl.cm.jet
+sc = plt.scatter(phistars, fes, c=peak_vars, cmap=cmap, norm=norm)
+plt.colorbar(sc)
+
+
+
+plt.close('all')
+for idx in [0,5,24]:
+    
+    this_phistar = phistars[sort_idx][idx]
+    this_fe = fes[sort_idx][idx]
+    this_nvphi = nvphis[sort_idx][idx]
+    this_fname = fnames[sort_idx][idx]
+    this_ndat = ndats[sort_idx][idx]
+
+    print("fname: {}  phistar: {:0.2f}  fe: {:0.2f}".format(this_fname, this_phistar, this_fe))
+
+    plt.plot(this_nvphi[:,0], this_nvphi[:,4], label=r'$\phi^*={:1.2f}$'.format(this_phistar))
+    #plt.plot(this_ndat[:,0], this_ndat[:,2], '-o', label=r'$\phi^*={:1.2f}$'.format(this_phistar))
+
+
