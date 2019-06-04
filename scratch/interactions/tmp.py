42da69ae4d3bdecffebc526c011828c45732b6da
Nick Reg <nrego@med.upenn.edu>
2019-06-04 12:07:54 -0400

    tmp file

---
+++ scratch/interactions/tmp.py
@@ -0,0 +1,14 @@
+from matplotlib import rc
+import os, glob
+
+rc('text', usetex=False)
+
+fnames = sorted(glob.glob('run_*/dhdl.xvg'))
+
+for fname in fnames:
+    dat = np.loadtxt(fname, comments=['@','#'])
+    name = os.path.dirname(fname)
+    plt.plot(dat[:,1], '-o', label=name)
+
+plt.legend()
+plt.show()
