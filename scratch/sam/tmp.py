4bcfd9a3d4c788a2ca00c8e7f65b64facfbdd3f4
Nick Rego <nbrego2@gmail.com>
2020-01-08 16:39:25 -0500

    new temporary sam script tmp.py

--- scratch/sam/tmp.py
+++ scratch/sam/tmp.py
@@ -2,45 +2,35 @@ import os, glob
 import numpy as np
 from scratch.sam.util import *
 
-fnames = sorted(glob.glob('*.dat'))
+fnames = sorted(glob.glob('k_*/d_*/trial_0/PvN.dat')) + sorted(glob.glob('../inv_pattern_sample/l_*/d_*/trial_0/PvN.dat')) + sorted(glob.glob('k_*/PvN.dat'))
+ds = np.load("sam_pattern_data.dat.npz")
 
-def find_ext_edge(positions, pos_ext):
-    d, patch_indices = cKDTree(pos_ext).query(positions, k=1)
-    nn, nn_ext, dd, dd_ext = construct_neighbor_dist_lists(positions, pos_ext)
+energies_old = ds['energies']
+methyl_pos_old = ds['methyl_pos']
 
-    int_edges = dict()
-    ext_edges = dict()
-    for i in range(positions.shape[0]):
-        ext_edges[i] = 0
-        int_edges[i] = nn[i].size
+myfeat_old = np.zeros((len(energies_old), 3))
 
-        for j in nn_ext[i]:
-            if j not in patch_indices:
-                ext_edges[i] += 1
+for i, methyl_mask in enumerate(methyl_pos_old):
+    state = State(np.arange(36).astype(int)[methyl_mask])
 
+    myfeat_old[i] = state.k_o, state.n_oo, state.n_oe
 
-    ext_nodes = (np.array(list(ext_edges.values())) > 0).sum()
+perf_mse_old, err, xvals, fit, reg_old = fit_leave_one(myfeat_old, energies_old)
 
-    return int_edges, ext_edges, ext_nodes
+myfeat_new = np.zeros((len(fnames), 3))
+energies_new = np.zeros(len(fnames))
+energies_new[:] = -1
+energies_err = np.zeros_like(energies_new)
 
-ds = np.load('sam_pattern_data.dat.npz')
+for i, fname in enumerate(fnames):
+    pvn = np.loadtxt(fname)[0]
+    pt_idx = np.loadtxt('{}/this_pt.dat'.format(os.path.dirname(fname)), dtype=int)
+    state = State(pt_idx)
 
-pos_ext = gen_pos_grid(12, z_offset=True, shift_y=-3, shift_z=-3)
+    myfeat_new[i] = state.k_o, state.n_oo, state.n_oe
+    energies_new[i] = pvn[1]
+    energies_err[i] = pvn[2]
 
-pos_66 = gen_pos_grid(6)
-pos_44 = gen_pos_grid(4)
-pos_22 = gen_pos_grid(2)
-pos_23 = gen_pos_grid(2,3)
-pos_49 = gen_pos_grid(4, 9, shift_z=-1.5, shift_y=1)
+perf_mse_new, err, xvals, fit, reg_new = fit_leave_one(myfeat_new, energies_new)
 
-int_edges66, ext_edges66, n_ext66 = find_ext_edge(pos_66, pos_ext)
-int_edges44, ext_edges44, n_ext44 = find_ext_edge(pos_44, pos_ext)
-int_edges22, ext_edges22, n_ext22 = find_ext_edge(pos_22, pos_ext)
-int_edges49, ext_edges49, n_ext49 = find_ext_edge(pos_49, pos_ext)
-int_edges23, ext_edges23, n_ext23 = find_ext_edge(pos_23, pos_ext)
-
-
-feat = np.array([[2,2], [2,3], [4,4], [6,6], [4,9]])
-ext = np.array([np.sum(list(ext_edges22.values())), np.sum(list(ext_edges23.values())), np.sum(list(ext_edges44.values())), np.sum(list(ext_edges66.values())), np.sum(list(ext_edges49.values()))])
-n_ext = np.array([n_ext22, n_ext23, n_ext44, n_ext66, n_ext49])
 
