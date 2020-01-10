2f20027a6fb747208e2d7280a3f628296a65bb4b
Nick Rego <nbrego2@gmail.com>
2020-01-10 15:01:37 -0500

    plotting sam stuff

--- scratch/sam/old/tmp.py
+++ scratch/sam/old/tmp.py
@@ -2,20 +2,23 @@ import os, glob
 import numpy as np
 from scratch.sam.util import *
 
-fnames = sorted(glob.glob('k_*/d_*/trial_0/PvN.dat')) + sorted(glob.glob('../inv_pattern_sample/l_*/d_*/trial_0/PvN.dat')) + sorted(glob.glob('k_*/PvN.dat'))
-ds = np.load("sam_pattern_data.dat.npz")
+#fnames = sorted(glob.glob('k_*/d_*/trial_0/PvN.dat')) + sorted(glob.glob('../inv_pattern_sample/l_*/d_*/trial_0/PvN.dat')) + sorted(glob.glob('k_*/PvN.dat'))
+fnames = sorted(glob.glob('*/d_*/trial_0/PvN.dat'))
+p = 4
+q = 9
+#ds = np.load("sam_pattern_data.dat.npz")
 
-energies_old = ds['energies']
-methyl_pos_old = ds['methyl_pos']
+#energies_old = ds['energies']
+#methyl_pos_old = ds['methyl_pos']
 
-myfeat_old = np.zeros((len(energies_old), 3))
+#myfeat_old = np.zeros((len(energies_old), 3))
 
-for i, methyl_mask in enumerate(methyl_pos_old):
-    state = State(np.arange(36).astype(int)[methyl_mask])
+#for i, methyl_mask in enumerate(methyl_pos_old):
+#    state = State(np.arange(36).astype(int)[methyl_mask])
 
-    myfeat_old[i] = state.k_o, state.n_oo, state.n_oe
+#    myfeat_old[i] = state.k_o, state.n_oo, state.n_oe
 
-perf_mse_old, err, xvals, fit, reg_old = fit_leave_one(myfeat_old, energies_old)
+#perf_mse_old, err, xvals, fit, reg_old = fit_leave_one(myfeat_old, energies_old)
 
 myfeat_new = np.zeros((len(fnames), 3))
 energies_new = np.zeros(len(fnames))
@@ -27,7 +30,7 @@ states = np.empty_like(energies_new, dtype=object)
 for i, fname in enumerate(fnames):
     pvn = np.loadtxt(fname)[0]
     pt_idx = np.loadtxt('{}/this_pt.dat'.format(os.path.dirname(fname)), dtype=int)
-    state = State(pt_idx)
+    state = State(pt_idx, p, q)
 
     myfeat_new[i] = state.k_o, state.n_oo, state.n_oe
     energies_new[i] = pvn[1]
@@ -35,5 +38,8 @@ for i, fname in enumerate(fnames):
     states[i] = state
 
 perf_mse_new, err, xvals, fit, reg_new = fit_leave_one(myfeat_new, energies_new)
+
+
+np.savez_compressed('sam_pattern_{:02d}_{:02d}'.format(p, q), states=states, energies=energies_new) 
 
 
--- scratch/sam/plot_m3.py
+++ scratch/sam/plot_m3.py
@@ -48,16 +48,49 @@ perf_mse, err_m1, xvals, fit, reg_m1 = fit_leave_one(myfeat[:,0].reshape(-1,1), 
 perf_mse, err, xvals, fit, reg = fit_leave_one(myfeat, energies)
 k_vals = myfeat[:,0]
 
+
+# Plot residuals v k_o
 plt.close('all')
 
-fig = plt.figure(figsize=(7,6))
+fig = plt.figure(figsize=(7,6.5))
 ax = fig.gca()
-ax.scatter(k_vals, err_m1, label='Model 1')
-ax.scatter(k_vals, err, label='Model 3')
+ax.plot(k_vals, err_m1, 'o', label='Model 1')
+ax.plot(k_vals, err, 'o', label='Model 3')
 ax.set_xticks([0,12,24,36])
-fig.legend()
+#fig.legend()
+#ax.set_ylim(100,110)
+#plt.axis('off')
 fig.tight_layout()
-fig.savefig('{}/Desktop/res_comparison.pdf'.format(homedir), transparent=True)
+fig.savefig('{}/Desktop/res_comparison_ko.pdf'.format(homedir), transparent=True)
+
+# Plot residuals v n_oo
+plt.close('all')
+
+fig = plt.figure(figsize=(7,6.5))
+ax = fig.gca()
+ax.plot(myfeat[:,1], err_m1, 'o', label='Model 1')
+ax.plot(myfeat[:,1], err, 'o', label='Model 3')
+#ax.set_xticks([0,12,24,36])
+#fig.legend()
+#ax.set_ylim(100,110)
+#plt.axis('off')
+fig.tight_layout()
+fig.savefig('{}/Desktop/res_comparison_noo.pdf'.format(homedir), transparent=True)
+
+# Plot residuals v n_oe
+plt.close('all')
+
+fig = plt.figure(figsize=(7,6.5))
+ax = fig.gca()
+ax.plot(myfeat[:,2], err_m1, 'o', label='Model 1')
+ax.plot(myfeat[:,2], err, 'o', label='Model 3')
+#ax.set_xticks([0,12,24,36])
+#fig.legend()
+#ax.set_ylim(100,110)
+#plt.axis('off')
+fig.tight_layout()
+fig.savefig('{}/Desktop/res_comparison_noe.pdf'.format(homedir), transparent=True)
+
 
 
 # Find errors of each model with k
@@ -72,4 +105,10 @@ for i in range(37):
     mask = bin_assign == i
     mse_with_k[i] = sq_err[mask].mean()
     mse_with_k_m1[i] = sq_err_m1[mask].mean()
+
+## Get reg coef errorbars from bootstrapping
+boot_intercept, boot_coef = fit_bootstrap(myfeat, energies)
+
+print("intercept: {:0.4f} ({:0.4f})".format(reg.intercept_, boot_intercept.std(ddof=1)))
+print("coefs: {} ({})".format(reg.coef_, boot_coef.std(axis=0, ddof=1)))
 
