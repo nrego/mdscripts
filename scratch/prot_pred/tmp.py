478f794826c5945993bd0b6486b85e60ae2e2a14
Nick Reg <nrego@med.upenn.edu>
2020-01-26 12:16:50 -0500

    blah

--- scratch/prot_pred/tmp.py
+++ scratch/prot_pred/tmp.py
@@ -4,13 +4,15 @@ import MDAnalysis
 import glob, os, pathlib
 from scipy.integrate import cumtrapz
 
-fnames = sorted(glob.glob('*/prot_contact/phi_sims/PvN.dat'))
+fnames = sorted(glob.glob('*/prot_contact/noq_phi_sims/PvN.dat'))
 
 
 name_lut = {
+    '1bmd': 'MDH',
     '1brs_bn': 'barnase',
     '1brs_bs': 'barstar',
     '1msb_pred': 'MBP',
+    '1ycr_mdm2': 'MDM2',
     '2mlt': 'MLT',
     '2tsc_pred': 'TS'
 }
@@ -21,10 +23,12 @@ for i, fname in enumerate(fnames):
     path = pathlib.Path(fname)
     name = name_lut[path.parts[0]]
     dirname = os.path.dirname(fname)
+    topdir = os.path.dirname(dirname)
 
     n0 = np.loadtxt('{}/NvPhi.dat'.format(dirname))[0,1]
 
     dat = np.loadtxt(fname)
+    dat = np.loadtxt('{}/phi_sims/PvN.dat'.format(topdir))
     n = dat[:,0]
     beta_G_N = dat[:,1]
 
@@ -33,5 +37,7 @@ for i, fname in enumerate(fnames):
     n_diff = n0 - n
     mask = n_diff > 0
     plt.plot(n_diff[mask], (beta_G_N/n_diff)[mask], label=name)
+
+
 plt.legend()
 plt.show()
--- scratch/sam/plot_transfer_intercept.py
+++ scratch/sam/plot_transfer_intercept.py
@@ -31,8 +31,9 @@ mpl.rcParams.update({'legend.fontsize':30})
 ds_bulk = np.load('sam_pattern_bulk_pure.npz')
 ds_pure = np.load('sam_pattern_pure.npz')
 
-# Skip pure hydroxyl
-slc = slice(1, None, 2)
+
+# Skip pure hydroxyl or methyl
+slc = slice(0, None, 2)
 energies_pure = ds_pure['energies'][slc]
 emin_pure = ds_pure['base_energy']
 p_q = ds_pure['pq'][slc].astype(float)
@@ -42,7 +43,7 @@ dy = ds_pure['dy'][slc]
 dz = ds_pure['dz'][slc]
 
 sa = dy*dz #+ 2*dx*(dy+dz)
-feat = np.vstack((dy*dz, dy, dz)).T
+feat_subvol = np.vstack((dy*dz, dy, dz)).T
 
 energies_bulk = ds_bulk['energies']
 errs_bulk = ds_bulk['err_energies']
@@ -54,51 +55,34 @@ assert np.array_equal(dy, ds_bulk['dy'])
 assert np.array_equal(dz, ds_bulk['dz'])
 
 
-#diffs = energies_pure - energies_bulk
-#plt.errorbar(sa, energies_bulk, yerr=errs_bulk, fmt='o')
-#plt.show()
+diffs = energies_pure - energies_bulk
 
-perf_mse_bulk, err_bulk, xvals, fit, reg_bulk = fit_leave_one(feat, energies_bulk, fit_intercept=False)
-alpha1, alpha2, alpha3 = reg_bulk.coef_ 
-plt.plot(xvals, fit)
-plt.errorbar(sa, energies_bulk, yerr=errs_bulk, fmt='o')
-plt.show()
+## Fit regressions of dy, dz on P and Q
+reg_p = linear_model.LinearRegression().fit(p_q[:,0].reshape(-1,1), dy)
+reg_q = linear_model.LinearRegression().fit(p_q[:,1].reshape(-1,1), dz)
+y0 = reg_p.intercept_
+z0 = reg_q.intercept_
 
+ly = reg_p.predict(p_q[:,0].reshape(-1,1))
+lz = reg_q.predict(p_q[:,1].reshape(-1,1))
 
-## Relate P,Q to dy, dz
-dq = 0.5
-dp = 0.5*np.sqrt(3)*dq
+dp = reg_p.coef_[0]
+dq = reg_q.coef_[0]
 
-y0 = np.mean(dy - dp*p_q[:,0])
-z0 = np.mean(dz - dq*p_q[:,1])
-
-reg_y = linear_model.LinearRegression()
-reg_y.fit(p_q[:,0].reshape(-1,1), dy)
-
-reg_z = linear_model.LinearRegression()
-reg_z.fit(p_q[:,1].reshape(-1,1), dz)
-
-y0 = reg_y.intercept_
-z0 = reg_z.intercept_
-dp = reg_y.coef_[0]
-dq = reg_z.coef_[0]
-
-ly = y0 + dp*p_q[:,0]
-lz = z0 + dq*p_q[:,1]
-lylz = y0*z0 + dp*dq*np.prod(p_q, axis=1) + z0*dp*p_q[:,0] + y0*dq*p_q[:,1]
-assert np.allclose(ly*lz, lylz)
-
-## Construct model with just P, Q
-feat_pq = np.zeros_like(feat)
-feat_pq[:,0] = np.prod(p_q, axis=1)
+feat_pq = np.zeros_like(feat_subvol)
+feat_pq[:,0] = p_q.prod(axis=1)
 feat_pq[:,1:] = p_q
-reg_pq = linear_model.LinearRegression()
-reg_pq.fit(feat, energies_bulk)
 
-reg_pq.intercept_ = alpha1*y0*z0 + alpha2*y0 + alpha3*z0
-reg_pq.coef_[0] = alpha1*dp*dq
-reg_pq.coef_[1] = dp*(alpha1*z0 + alpha2)
-reg_pq.coef_[2] = dq*(alpha1*y0 + alpha3)
+perf_mse_subvol, err_subvol, xvals, fit, reg_subvol = fit_leave_one(feat_subvol, energies_pure, fit_intercept=False)
+a1, a2, a3 = reg_subvol.coef_
+perf_mse_pq, err_pq, xvals, fit, reg_pq = fit_leave_one(feat_pq, energies_pure-emin_pure, fit_intercept=False)
 
+reg_temp = linear_model.LinearRegression().fit(feat_pq, energies_pure)
+reg_temp.intercept_ = a1*y0*z0 + a2*y0 + a3*z0
+reg_temp.coef_[0] = a1*dp*dq
+reg_temp.coef_[1] = (a1*z0 + a2)*dp
+reg_temp.coef_[2] = (a1*y0 + a3)*dq
 
-
+pred = reg_temp.predict(feat_pq)
+err = energies_pure - pred
+perf_mse = err**2
