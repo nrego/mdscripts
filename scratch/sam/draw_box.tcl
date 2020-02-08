234a93289c3dac94f87451c387c25bb496598ddb
Nick Reg <nrego@med.upenn.edu>
2020-02-08 14:28:33 -0500

    draw both sides of box for sam draw_box.tcl; change to surface area in plot_transfer_intercept.py

--- scratch/sam/draw_box.tcl
+++ scratch/sam/draw_box.tcl
@@ -6,4 +6,14 @@ proc draw_box {xmin ymin zmin xmax ymax zmax} {
     draw line "$xmin $ymin $zmin" "$xmin $ymax $zmin"
     draw line "$xmin $ymin $zmax" "$xmin $ymax $zmax"
     draw line "$xmin $ymax $zmin" "$xmin $ymax $zmax"
+
+    draw line "$xmax $ymin $zmin" "$xmax $ymin $zmax"
+    draw line "$xmax $ymin $zmin" "$xmax $ymax $zmin"
+    draw line "$xmax $ymin $zmax" "$xmax $ymax $zmax"
+    draw line "$xmax $ymax $zmin" "$xmax $ymax $zmax"
+
+    draw line "$xmin $ymin $zmin" "$xmax $ymin $zmin"
+    draw line "$xmin $ymin $zmax" "$xmax $ymin $zmax"
+    draw line "$xmin $ymax $zmin" "$xmax $ymax $zmin"
+    draw line "$xmin $ymax $zmax" "$xmax $ymax $zmax"
 }
