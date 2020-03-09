eb17da713fd5d802cf1060a9a6c6be136af6199b
Nick Reg <nrego@med.upenn.edu>
2020-03-08 23:42:02 -0400

    blah

--- scratch/sam/tmp_count_multiplicity.py
+++ scratch/sam/tmp_count_multiplicity.py
@@ -115,7 +115,7 @@ def enumerate_states(x, delta, mode='build_phil'):
 
     return all_states
 
-print("ENUMERATING STATES FOR MODE {} P: {} Q: {}, index: {}\n".format(mode, p, q))
+print("ENUMERATING STATES FOR MODE {} P: {} Q: {}\n".format(mode, p, q))
 print("...enumerating states...")
 all_states = enumerate_states(x0, delta, mode=mode)
 print("...Done\n")
