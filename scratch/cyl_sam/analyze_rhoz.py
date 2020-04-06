982fb2dc9a8be0bb28bdfd1501af0fbf027dbffe
Nick Rego <nickrego@nick-iMac.local>
2020-04-06 19:24:24 -0400

    analysis

---
+++ scratch/cyl_sam/analyze_rhoz.py
@@ -0,0 +1,113 @@
+from __future__ import division; __metaclass__ = type
+import sys
+import numpy as np
+from math import sqrt
+import argparse
+import logging
+import shutil
+import MDAnalysis
+#from MDAnalysis.coordinates.xdrfile.libxdrfile2 import read_xtc_natoms, xdrfile_open
+
+from IPython import embed
+
+from scipy.spatial import cKDTree
+import itertools
+#from skimage import measure
+
+from rhoutils import rho, cartesian
+from mdtools import ParallelTool
+
+from constants import SEL_SPEC_HEAVIES, SEL_SPEC_HEAVIES_NOWALL
+from mdtools.fieldwriter import RhoField
+import sys
+import argparse, os
+from scipy.interpolate import interp2d
+
+import matplotlib as mpl
+mpl.rcParams.update({'axes.labelsize': 30})
+mpl.rcParams.update({'xtick.labelsize': 30})
+mpl.rcParams.update({'ytick.labelsize': 30})
+mpl.rcParams.update({'axes.titlesize': 30})
+
+xmin = 28.0
+ymin = 15.0
+zmin = 15.0
+
+xmax = 38.5
+ymax = 55.0
+zmax = 55.0
+
+box_vol = (xmax-xmin)*(ymax-ymin)*(zmax-zmin)
+
+avg_n0 = np.load("cube_data_equil.dat.npz")['n0'].item()
+rho_0 = avg_n0 / box_vol
+rho_vols = np.load("rho_vols.dat.npy") 
+expt_waters = rho_0 * rho_vols
+
+print("{:.2f} waters in V ({} A); density: {:.4e}\n".format(avg_n0, box_vol, rho_0))
+
+ds = np.load('rhoz_final.dat.npz')
+
+rhoz = ds['rhoz']
+xvals = ds['xvals']
+rvals = ds['rvals']
+xx = ds['xx']
+rr = ds['rr']
+beta_phi_vals = ds['beta_phi_vals']
+
+# At bphistar=0.7
+
+rr = rr / 10.0
+xx = (xx - 28.5) / 10.0
+
+new_r = np.arange(0, 1.6, 0.005)
+new_x = np.arange(0, 0.5, 0.005)
+new_rr, new_xx = np.meshgrid(new_r, new_x, indexing='ij')
+
+levels = np.linspace(0,1,3)
+norm = plt.Normalize(0,1)
+cmap = 'bwr_r'
+
+homedir = os.environ['HOME']
+
+def plot_it(idx):
+    this_rho = rhoz[idx]
+    this_rho /= expt_waters
+    
+    # Interpolate
+    #f = interp2d(rr, xx, this_rho.T, kind='cubic')
+    #smooth_rho = f(new_r, new_x)
+
+    plt.close('all')
+    fig, ax = plt.subplots(figsize=(10,6))
+
+    pc = ax.pcolormesh(rr, xx, this_rho.T, cmap=cmap, norm=norm, shading='gourand')
+    fig.colorbar(pc)
+    ax.contour(rr, xx, this_rho.T, levels=[0.5], color='k')
+    
+    ax.set_ylim(0, 0.4)
+    ax.set_xlim(0, 1.5)
+    ax.plot([0,1.5], [0.3,0.3], 'g--', linewidth=4)
+    ax.set_xlabel(r'$r$ (nm)')
+    ax.set_ylabel(r'$z$ (nm)')
+    if idx == 35:
+        label = r'$\beta \phi^*={:.2f}$'.format(beta_phi_vals[idx])
+        ax.set_title(label)
+
+        plt.tight_layout()
+        plt.savefig('{}/Desktop/snap_{:03d}'.format(homedir, idx))
+
+        [shutil.copy('{}/Desktop/snap_{:03d}.png'.format(homedir, idx), '{}/Desktop/snap_{:03d}_{:02d}.png'.format(homedir, idx, i)) for i in range(10)]
+    else:
+        label = r'$\beta \phi={:.2f}$'.format(beta_phi_vals[idx])
+        ax.set_title(label)
+
+        plt.tight_layout()
+        plt.savefig('{}/Desktop/snap_{:03d}'.format(homedir, idx))
+
+for i in range(40):
+    plot_it(i)
+
+
+
+
