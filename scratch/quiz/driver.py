9f64942dcaf299556aebab53ee1b3f9c39f52580
Nick Reg <nrego@med.upenn.edu>
2020-04-11 19:09:21 -0400

    blah

---
+++ scratch/quiz/driver.py
@@ -0,0 +1,66 @@
+# Nbr 05/30/19
+# (use %paste in IPython)
+
+from __future__ import division, print_function
+
+import numpy as np
+import MDAnalysis
+from scipy.spatial import cKDTree
+from scratch.quiz.lib import *
+
+
+n_rounds = 4
+# shape : n_rounds, n_qs
+questions = {
+    0: ["How are you doing today?", 
+        "What's your favorite color?",
+        "Tell me about someone at work that annoys you.",
+        "How do you feel about wealth inequality?",
+        "Charter schools: Good things or bad things?"
+        ],
+    # Gray round
+    1: ["What is the main character of '50 shades of gray' named?",
+        "Name as many animals as you can that have the word 'gray' in their name (up to 3)",
+        "Gray-scale pixels are encoded by one number - color by 3. what are the 3 numbers?",
+        "Dogs can only see in black and white: true or false?",
+        "Name a type of cloud."
+        ],
+    # "General" knowledge
+    2: ["In the punic wars, which general suprised romans...",
+        "Name as many us companies as you can that have the word 'general' in them (up to 3)",
+        "Complete the sentence: 'Hello there!'  'General ____!'",
+        "Latin root of 'general' means 'origin, type, group', or 'race'. what is the root?",
+        "Famous French generals (or war leaders)",
+        "Beyonce debut"
+        ],
+
+    # "FOOOD"
+    3: ["Shakshuka",
+        "Durian",
+        "Mex natl dish",
+        "masala refers to what?",
+        "This famous italian dish is prepared with capers, anchovies, and olives",
+        "'Vindaloo' comes from the Portueguese - 'vin' and 'aloo' - what does it mean? one pt each"
+        ],
+
+    # Psychological torture
+    4: ["Produce a red sock.",
+        "Two ropes each takes 1 hr to burn, but not at same rate. how to measure 45 min?",
+        "Make me a fucking sandwich",
+        "Riddle: I am a chest with no edges, hinges, no key, no lid, yet within me golden treasure's hid. what am i?",
+        "You have an aquarium with 200 fish. 99pc of them are red. How many do you have to remove to have 98pc red fish?",
+        "Elevator question: man on 10th floor takes it down, only takes it up to 7th floor. why?",
+        "I can only be kept if I am given. To some people, I am strong as iron. To others, I am weak as smoke.",
+        "Riddle: This thing all things devours: birds, beasts, trees, flowers; gnaws iron, bites steal, grinds hard stone to meal. slays kings, ruins town, and beats high mountain down"]
+
+}
+
+
+
+#q1.add_score(1,1)
+
+qm = QuizzMaster()
+
+
+qm.init_quizzers('responses.csv')
+
---
+++ scratch/quiz/lib.py
@@ -0,0 +1,103 @@
+# Nbr 05/30/19
+# (use %paste in IPython)
+
+from __future__ import division, print_function
+
+import numpy as np
+import MDAnalysis
+from scipy.spatial import cKDTree
+from matplotlib import pyplot as plt
+
+## Keeps track of team name, score
+class Quizz:
+
+    def __init__(self, team_name):
+        self.name = team_name
+
+        self.scores = np.zeros(4)
+
+
+    def get_scores(self):
+        return self.scores
+
+    def get_score(self, round):
+        return self.scores[round]
+
+    def get_score_upto(self, round):
+        return np.sum(self.scores[round])
+
+    def add_score(self, round, inc):
+        self.scores[round] += inc
+
+
+## Holds all the quizzers, can plot all scores on the fly
+class QuizzMaster:
+
+    def __init__(self):
+        self.quizzers = dict()
+
+    def add_quizzer(self, quizzer):
+        self.quizzers[quizzer.name] = quizzer
+
+    # Load in round 0, init quizzers
+    def init_quizzers(self, infile):
+        txt = np.loadtxt(infile, dtype=str)
+
+        for in_str in txt:
+            in_str = in_str[0]
+            splits = in_str.split(sep)
+            team_name = splits[1]
+            print("found team name : {}. adding...".format(team_name))
+
+            q = Quizz(team_name)
+            self.add_quizzer(q)
+
+    def collect_and_assign(self, questions, round, infile, sep=','):
+
+        this_qs = questions[round]
+
+        txt = np.loadtxt(infile, dtype=str)
+
+        for in_str in txt:
+            in_str = in_str[0]
+            splits = in_str.split(sep)
+            team_name = splits[1]
+            self.print_names()
+            team_idx = int(input("select team for {} (-1 to skip):  ".format(team_name)))
+            if team_idx == -1:
+                continue
+
+            team_name = list(self.quizzers.keys())[team_idx]
+            this_q = self.quizzers[team_name]
+            print(" selected team : {}".format(team_name))
+            
+
+            ## Now loop thru q's
+            for iq, question in enumerate(questions[round]):
+                answer = splits[iq+2]
+                print("Q: {}  [A: {}]".format(question, answer))
+
+                points = int(input("    points? : "))
+                this_q.add_score(round, points)
+
+    def print_names(self):
+
+        for i, name in enumerate(self.quizzers.keys()):
+            print('{:02d}: {}\n'.format(i, name))
+
+
+    # Sort quizzers by score, plot 
+    def sort_and_plot(self, round):
+
+        this_quizzers = np.array(list(self.quizzers.keys()))
+        this_scores = np.array([q.get_score_upto(round) for q in self.quizzers.values()])
+        print("Scores: {}".format(this_scores))
+        sort_idx = np.argsort(this_scores)[::-1]
+        indices = np.arange(len(this_quizzers))
+
+        fig, ax = plt.subplots()
+        ax.bar(indices, this_scores[sort_idx], width=1)
+        ax.set_xticks(indices)
+        ax.set_xticklabels(this_quizzers[sort_idx])
+
+        plt.show()
