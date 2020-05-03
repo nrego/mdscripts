f88e00333666bc8028e36c7955cdbcd9a6e9c13e
Nick Rego <nickrego@nick-iMac.local>
2020-05-03 11:28:57 -0400

    1 conv operation prior to filters

--- scratch/sam/gen_figs/plot_m1.py
+++ scratch/sam/gen_figs/plot_m1.py
@@ -46,7 +46,6 @@ print('  ...Done\n')
 n_dat = energies.size
 indices = np.arange(n_dat)
 
-
 perf_mse, err, xvals, fit, reg = fit_leave_one(k_o, energies, fit_intercept=True)
 boot_int, boot_coef = fit_bootstrap(k_o, energies, fit_intercept=True)
 
@@ -68,94 +67,31 @@ norm = plt.Normalize(-15,15)
 err_kwargs = {"lw":.5, "zorder":0, "color":'k'}
 
 #sc = ax.scatter(k_o, energies, s=50, c=norm(err), cmap='coolwarm_r', zorder=100)
-sc = ax.scatter(k_o, energies, s=50, zorder=100, color='k')
+sc = ax.scatter(k_o, energies, s=50, zorder=100, color='k', label=r'$f$')
 #ax.errorbar(k_o, energies, errs, fmt='none', **err_kwargs)
-ax.plot(xvals, fit, '-', linewidth=4, zorder=200)    
+ax.plot(xvals, fit, '-', linewidth=4, zorder=200, label='linear')    
 
-ax.plot(xvals, fit_poly, '-', linewidth=4, zorder=300)
+ax.plot(xvals, fit_poly, '-', linewidth=4, zorder=300, label='quadratic')
 
 ax.set_xticks([0,12,24,36])
 
 fig.tight_layout()
 fig.savefig('{}/Desktop/m1.pdf'.format(homedir), transparent=True)
+ax.set_ylim(-100,-50)
+plt.legend()
+fig.savefig('{}/Desktop/m1_label.pdf'.format(homedir), transparent=True)
 
 
-## Horizontal error slice (different k vals, but similar delta f)
-##########################
-
-e_val = -61
-if do_constr:
-    e_val = e_val - f_c
-    diff = np.abs(delta_f - e_val)
-else:
-    diff = np.abs(dg_bind - e_val)
-
-diff_mask = diff < 2
-min_k = myfeat[diff_mask, 0].min()
-max_k = myfeat[diff_mask, 0].max()
-mid_k = np.round((e_val - reg.intercept_) / (reg.coef_[0]))
-min_mask = diff_mask & (myfeat[:,0] == min_k)
-max_mask = diff_mask & (myfeat[:,0] == max_k)
-mid_mask = diff_mask & (myfeat[:,0] == mid_k)
-
-min_idx = indices[min_mask][diff[min_mask].argmin()]
-max_idx = indices[max_mask][diff[max_mask].argmin()]
-mid_idx = indices[mid_mask][diff[mid_mask].argmin()]
-
-
-min_state = states[min_idx]
-max_state = states[max_idx]
-mid_state = states[mid_idx]
+#### PLOT ERRORS VS KO ####
 
 plt.close('all')
-min_state.plot()
-plt.savefig('{}/Desktop/fig_min_k.pdf'.format(homedir), transparent=True)
-plt.close('all')
-max_state.plot()
-plt.savefig('{}/Desktop/fig_max_k.pdf'.format(homedir), transparent=True)
-plt.close('all')
-mid_state.plot()
-plt.savefig('{}/Desktop/fig_mid_k.pdf'.format(homedir), transparent=True)
-
-plt.close('all')
-
-
-### Vertical err - variety of delta_f's for patterns with same k_o
-######################
-k_val = 23
-k_mask = myfeat[:,0] == k_val
-pred_e = reg.predict(np.array([k_val]).reshape(1,-1)).item()
-
-if do_constr:
-    delta_f[k_mask] - pred_e
-else:
-    diff = dg_bind[k_mask] - pred_e
-
-min_idx = indices[k_mask][diff.argmin()]
-max_idx = indices[k_mask][diff.argmax()]
-mid_idx = indices[k_mask][np.abs(diff).argmin()]
-
-min_state = states[min_idx]
-max_state = states[max_idx]
-mid_state = states[mid_idx]
-
-plt.close('all')
-min_state.plot()
-plt.savefig('{}/Desktop/fig_min_e.pdf'.format(homedir), transparent=True)
-plt.close('all')
-max_state.plot()
-plt.savefig('{}/Desktop/fig_max_e.pdf'.format(homedir), transparent=True)
-plt.close('all')
-mid_state.plot()
-plt.savefig('{}/Desktop/fig_mid_e.pdf'.format(homedir), transparent=True)
-
-plt.close('all')
-
 fig = plt.figure(figsize=(7,6))
-pred = reg.predict(myfeat)
 ax = fig.gca()
-ax.plot(pred, delta_f, 'ok')
-ax.plot([0, f_o-f_c], [0, f_o-f_c], 'k-', linewidth=4)
-plt.savefig('{}/Desktop/fig_m1_parity.pdf'.format(homedir), transparent=True)
+
+sc = ax.scatter(k_o, err)
+
+ax.scatter(k_o, err_poly)
+ax.set_xticks([0,12,24,36])
+fig.savefig('{}/Desktop/m1_err.pdf'.format(homedir), transparent=True)
 
 
