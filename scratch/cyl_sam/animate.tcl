f943bb28b5954df78f5e0d98e5d0240de3c56867
Nick Rego <nickrego@client0487-hnt.vpn.private.upenn.edu>
2020-07-13 20:45:18 -0400

    blah

---
+++ scratch/cyl_sam/animate.tcl
@@ -0,0 +1,25 @@
+#
+
+proc animate_mov {fileformat} {
+
+
+    set num [molinfo top get numframes]
+    
+
+    for {set i 0} {$i < $num} {incr i 1} {
+        draw delete all
+        animate goto $i
+
+        draw cylinder {28.5 35 35} {37.5 35 35} radius 20. resolution 1000 filled yes
+        draw material Transparent
+        display update        
+        set outfile [format $fileformat [expr $i]]
+
+        puts "Rendering $outfile"
+
+        render snapshot $outfile
+
+    }
+    
+
+}
