9b1f16493c2faae8a4d772baeaf8c498d78c9511
Nick Rego <nickrego@client0054vag.vpn.private.upenn.edu>
2020-07-30 22:39:23 -0400

    blah

--- scratch/sam/run_wl_hotspot.py
+++ scratch/sam/run_wl_hotspot.py
@@ -188,5 +188,5 @@ kwargs = {
 wl = WangLandau(state_po.positions, bins, fn=get_order, fn_kwargs=kwargs, eps=1e-6, max_iter=30000)
 wl.gen_states(k=k_o, do_brute=(not do_wl))
 
-np.savez_compressed('hotspot_dos_p_{:02d}_q_{:02d}_ko_{:03d}'.format(p,q,k_o), wl=wl,
-                     p=p, q=q, ko=k_o)
+np.savez_compressed('hotspot_dos_p_{:02d}_q_{:02d}_ko_{:03d}'.format(p,q,k_o), sampled_points=wl.sampled_pt_idx,
+                     bins=wl.bins, entropies=wl.entropies, density=wl.density, p=p, q=q, ko=k_o)
